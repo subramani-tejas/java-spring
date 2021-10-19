a295cafb61c23e047f61a73e43116ea375c0d745
Tejas <last44705@gmail.com>
2021-10-19 11:57:10 -0500

    test commit

--- src/main/java/com/codewithtejas/springcore/SpringCoreApplication.java
+++ src/main/java/com/codewithtejas/springcore/SpringCoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCoreApplication {
 
+	// test commit
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreApplication.class, args);
 	}
